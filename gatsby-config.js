86d440463bc499c45ed28cd17d89efbb3326374b
Max Gilbert <mgilbert@aucklandmuseum.com>
2022-04-29 15:47:39 +1200

    Try contentful sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,24 @@ module.exports = {
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
-    "gatsby-transformer-remark",
-    "gatsby-transformer-sharp",
+      `gatsby-plugin-sharp`,
+      {
+          resolve: `gatsby-transformer-remark`,
+          options: {
+              plugins: [
+                  {
+                      resolve: `gatsby-remark-images-contentful`,
+                      options: {
+                          // It's important to specify the maxWidth (in pixels) of
+                          // the content container as this plugin uses this as the
+                          // base for generating different widths of each image.
+                          maxWidth: 590,
+                      },
+                  },
+              ],
+          },
+      },
+      "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
