e39ac15e12fca3d23feb3a6124e7dd12dbd78b6f
Max Gilbert <mgilbert@aucklandmuseum.com>
2022-05-25 15:18:57 +1200

    Shopify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
 });
 
 const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken:
-    process.env.CONTENTFUL_ACCESS_TOKEN ||
-    process.env.CONTENTFUL_DELIVERY_TOKEN,
+    spaceId: process.env.CONTENTFUL_SPACE_ID,
+    accessToken:
+        process.env.CONTENTFUL_ACCESS_TOKEN ||
+        process.env.CONTENTFUL_DELIVERY_TOKEN,
 };
 
 // If you want to use the preview API please define
@@ -22,51 +22,60 @@ const contentfulConfig = {
 //
 // To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
 if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST;
-  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+    contentfulConfig.host = process.env.CONTENTFUL_HOST;
+    contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
 }
 
 const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the access token need to be provided."
-  );
+    throw new Error(
+        "Contentful spaceId and the access token need to be provided."
+    );
 }
 
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
-  },
-  pathPrefix: "/gatsby-contentful-starter",
-  plugins: [
-      {
-          resolve: `gatsby-transformer-remark`,
-          options: {
-              plugins: [
-                  {
-                      resolve: `gatsby-remark-images-contentful`,
-                      options: {
-                          // It's important to specify the maxWidth (in pixels) of
-                          // the content container as this plugin uses this as the
-                          // base for generating different widths of each image.
-                          maxWidth: 768,
-                          withWebp: true,
-                          showCaptions: true,
-                          linkImagesToOriginal: false
-                      },
-                  },
-              ],
-          },
-      },
-      "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-source-contentful",
-      options: contentfulConfig,
+    siteMetadata: {
+        title: "Gatsby Contentful Starter",
+        description: "Official Contentful Gatsby Starter",
     },
-  ],
+    pathPrefix: "/gatsby-contentful-starter",
+    plugins: [
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images-contentful`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 768,
+                            withWebp: true,
+                            showCaptions: true,
+                            linkImagesToOriginal: false
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: "gatsby-source-shopify",
+            options: {
+                password: process.env.SHOPIFY_APP_PASSWORD,
+                storeUrl: process.env.GATSBY_MYSHOPIFY_URL,
+                shopifyConnections: ["collections"]
+            }
+        },
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-breakpoints",
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-image",
+        {
+            resolve: "gatsby-source-contentful",
+            options: contentfulConfig,
+        },
+    ],
 };
